67f22edd7ea3ba43743a2931c46b80e6c5e8263a
Shahryar <Shahryar.sky2014@gmail.com>
2026-02-16 00:05:53 -0800

    Delete crudly directory

--- crudly/src/Filters/globalFilter.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Shahrakii\crudly\Filters;
-
-return [
-    'id',
-    'created_at',
-    'updated_at',
-];
--- crudly/src/Filters/tableFilter.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Shahrakii\crudly\Filters;
-
-return [
-    'SomeTable' => ['SomeField1', 'SomeField2'],
-];
